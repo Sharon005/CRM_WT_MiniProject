b79e285d3b42ebf4750442bf0f4951394e36e8f9
phoneix-007 <spandanthakor@gmail.com>
2021-10-21 23:39:58 +0530

    random2

--- pages/view_customer.php
+++ pages/view_customer.php
@@ -1,6 +1,15 @@
 <?php include ('header.php'); ?>
-<?php include ('../includes/app.php');?>
-<div class="row justify-content-center">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+	<div class="row justify-content-center">
 	<div class="col-sm-9  pb-4">
 		<h3 class="text-center">View Customer</h3>
 		<table class="table table-bordered table-hover text-center table-responsive">
@@ -38,7 +47,10 @@
 		</table>
 	</div>
 </div>
-<?php include('../includes/footer.php'); ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
+
 <?php
 if($_SERVER['REQUEST_METHOD']=='POST'){
 	$c_name = mysqli_real_escape_string($con,$_POST['c_name']);
