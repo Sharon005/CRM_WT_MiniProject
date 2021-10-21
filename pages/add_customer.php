d0887d30c43a2677517ae412ef3c923e02ceb891
Sharon005 <sharonts123@gmail.com>
2021-10-21 23:41:22 +0530

    made changes in php

--- pages/add_customer.php
+++ pages/add_customer.php
@@ -34,67 +34,68 @@
     
             <div class="row">
                 <div class="col">
-                    <div class="v-nav">
-                        <ul class="nav flex-column">
-                            <li class="nav-item">
-                                <div class="btn-group">
-                                    <button id="b1" type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                                      Plans
-                                    </button>
-                                    <span class="visually-hidden">Toggle Dropdown</span>
-                                    <ul class="dropdown-menu dropdown-menu-dark">
-                                        <li><a class="dropdown-item" href="add_plan.html">Add Plans</a></li>
-                                        <li><a class="dropdown-item" href="">Delete Plans</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <br>
-                
-                            <li class="nav-item">
-                                <div class="btn-group ">
-                                    <button id="b1" type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                                      Employees
-                                    </button>
-                                    <ul class="dropdown-menu dropdown-menu-dark">
-                                        <li><a class="dropdown-item" href="add_employee.html">Add Employee</a></li>
-                                        <li><a class="dropdown-item" href="view_emplyee.html">View Employee</a></li>
-                                        <li><a class="dropdown-item" href="#">Edit Employee</a></li>
-                                        <li><a class="dropdown-item" href="#">Delete Employee</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <br>
-                
-                            <li class="nav-item">
-                                <div class="btn-group">
-                                    <button id="b1" type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                                      Customers
-                                    </button>
-                                    <ul class="dropdown-menu dropdown-menu-dark">
-                                        <li><a class="dropdown-item" href="add_customer.html">Add Customers</a></li>
-                                        <li><a class="dropdown-item" href="#">View Customers</a></li>
-                                        <li><a class="dropdown-item" href="#">Edit Customers</a></li>
-                                        <li><a class="dropdown-item" href="#">Delete Customers</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                
-                            <br>
-                            <li class="nav-item">
-                                <div class="btn-group">
-                                    <button id="b1" type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                                      Complaints
-                                    </button>
-                                    <ul class="dropdown-menu dropdown-menu-dark">
-                                      <li><a class="dropdown-item" href="add_complaint.html">Add Complaints</a></li>
-                                      <li><a class="dropdown-item" href="#">View Complaints</a></li>
-                                      <li><a class="dropdown-item" href="#">Edit Complaints</a></li>
-                                      <li><a class="dropdown-item" href="#">Delete Complaints</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                          </ul>
-                    </div>
+                <div class="v-nav">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <div class="btn-group">
+                            <button id="b1" type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                Plans
+                            </button>
+                            <span class="visually-hidden">Toggle Dropdown</span>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a class="dropdown-item" href="add_plan.php">Add Plans</a></li>
+                                <li><a class="dropdown-item" href="plan_edit.php">Edit Plans</a></li>
+                                <li><a class="dropdown-item" href="plan_view.php">View Plans</a></li>
+                                <li><a class="dropdown-item" href="plan_delete.php">Delete Plans</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <br>
+
+          <li class="nav-item">
+              <div class="btn-group ">
+                  <button id="b1" type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    Employees
+                  </button>
+                  <ul class="dropdown-menu dropdown-menu-dark">
+                      <li><a class="dropdown-item" href="add_employee.php">Add Employee</a></li>
+                      <li><a class="dropdown-item" href="viewemp.php">View Employee</a></li>
+                      <li><a class="dropdown-item" href="editemp.php">Edit Employee</a></li>
+                      <li><a class="dropdown-item" href="delete_emp.php">Delete Employee</a></li>
+                  </ul>
+              </div>
+          </li>
+          <br>
+
+          <li class="nav-item">
+              <div class="btn-group">
+                  <button id="b1" type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    Customers
+                  </button>
+                  <ul class="dropdown-menu dropdown-menu-dark">
+                      <li><a class="dropdown-item" href="add_customer.php">Add Customers</a></li>
+                      <li><a class="dropdown-item" href="view_customer.php">View Customers</a></li>
+                      <li><a class="dropdown-item" href="edit_customer.php">Edit Customers</a></li>
+                      <li><a class="dropdown-item" href="delete_customer.php">Delete Customers</a></li>
+                  </ul>
+              </div>
+          </li>
+
+          <br>
+          <li class="nav-item">
+              <div class="btn-group">
+                  <button id="b1" type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    Complaints
+                  </button>
+                  <ul class="dropdown-menu dropdown-menu-dark">
+                    <li><a class="dropdown-item" href="add_complaint.php">Add Complaints</a></li>
+                    <li><a class="dropdown-item" href="view_complaint.php">View Complaints</a></li>
+                    <li><a class="dropdown-item" href="solve_complaint.php">Solve Complaints</a></li>
+                  </ul>
+              </div>
+          </li>
+        </ul>
+  </div>
                 </div>
                 <div class="col">
                     <h2>Add Customer</h2>
